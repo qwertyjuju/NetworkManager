65d1db5b505aed4829ac8309fac25f50bd515f37
qwertyjuju <70114133+qwertyjuju@users.noreply.github.com>
2022-03-19 00:51:00 +0100

    Added PyQt
    added PyQt5 + ui file

--- Network_creator.py
+++ Network_creator.py
@@ -1,6 +1,25 @@
+import sys
 import ipaddress
-import serial
+from pathlib import Path
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt5 import uic
 
+class CreationTool:
+    def __init__(self, uifile):
+        self.app = QApplication(sys.argv)
+        Ui, Window = uic.loadUiType(uifile)
+        self.window = Window()
+        self.ui = Ui()
+        self.ui.setupUi(self.window)
+        self.init_ui()
+        self.window.show()
+        self.app.exec_()
+
+    def init_ui(self):
+        self.ui.B_CreateNetwork.clicked.connect(self.test)
+
+    def test (self):
+        print("test")
 
 class Network:
     def __init__(self, ipadd, nb_subnets=None):
@@ -16,13 +35,20 @@ class NetworkDevice:
     _counter = 0
 
     def __init__(self):
-        self._ID=self._counter
-        NetworkDevice._counter+=1
+        self._ID = self._counter
+        NetworkDevice._counter += 1
 
 
 class Switch(NetworkDevice):
-    pass
+    def __init__(self):
+        super().__init__()
+        pass
+
 
 
 def main():
     pass
+
+
+if __name__ == "__main__":
+    CreationTool(Path("data/ui/creation_tool.ui"))
